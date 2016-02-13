7fd1302ee288a4a7b184d872e25390313368fd02
Kurt Junshean Espinosa <kpespinosa@up.edu.ph>
2016-02-13 16:41:01 +0800

    added initial project files

---
+++ main.js
@@ -0,0 +1,4 @@
+var index = Math.floor(Math.random() * (quotes.length - 1));
+var quote = quotes[index];
+
+document.write(quote.content);
---
+++ quotes.js
@@ -0,0 +1,16 @@
+var quotes = [
+    {
+        content: 'lorem ipsum dolor sit amet',
+        author: 'random person'
+    },
+
+    {
+        content: 'another random quote',
+        author: 'another random person'
+    },
+
+    {
+        content: 'this is a sample quote',
+        author: 'sample author'
+    }
+];
