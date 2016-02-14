464602042fcd61098d1f6c4ac170a6e2a258e0fa
Jhon Christian Ambrad <jambrad123@gmail.com>
2016-02-14 19:31:52 +0800

    Added more quotes

--- javascripts/quotes.js
+++ javascripts/quotes.js
@@ -27,5 +27,45 @@ var quotes = [
     {
         content: 'Love is the poetry of the senses.',
         author: 'Honore de Balzac'
+    },
+
+    {
+        content: 'Love is a deeper season than reason; my sweet one'
+        author: 'EE Cummings'
+    },
+
+    {
+        content: 'The greatest happiness of life is the conviction that we are loved; loved for ourselves, or rather, loved in spite of ourselves'
+        author: 'Victor Hugo'
+    },
+
+    {
+        content: 'What we have once enjoyed we can never lose. All that we love deeply becomes a part of us.'
+        author: 'Helen Keller'
+    },
+
+    {
+        content: 'To those who have given up on love: I say, "Trust life a little bit."'
+        author: 'Maya Angelou'
+    },
+
+    {
+        content: 'You must love in such a way that the person you love feels free.'
+        author: 'Thich Nhat Hanh'
+    },
+
+    {
+        content: 'Trust your heart if the seas catch fire, live by love though the stars walk backward.'
+        author: 'EE Cummings'
+    },
+
+    {
+        content: 'Love is the most beautiful thing to have, hardest thing to earn and most painful thing to lose.'
+        author: 'Anonymous'
+    },
+
+    {
+        content: "Love seeketh not itself to please, nor for itself hath any care, but for another gives its ease, and builds a Heaven in Hell's despair"
+        author: 'Unknown'
     }
 ];
